bc9c24a18d0c30bfe62b3ec7ab77cb42c1f5d221
Paulo Roberto Nunes da Rosa <cpte.paulornr@gmail.com>
2018-06-29 11:25:26 -0300

    getter e setters implementados na classe registradores

--- src/Registradores.java
+++ src/Registradores.java
@@ -9,4 +9,53 @@ public class Registradores{
     public Registradores(){
         this.SR=0;
     }
+
+    public int getAX() {
+        return AX;
+    }
+
+    public int getDX() {
+        return DX;
+    }
+
+    public int getIP() {
+        return IP;
+    }
+
+    public int getSI() {
+        return SI;
+    }
+
+    public int getSP() {
+        return SP;
+    }
+
+    public int getSR() {
+        return SR;
+    }
+
+    public void setAX(int AX) {
+        this.AX = AX;
+    }
+
+    public void setDX(int DX) {
+        this.DX = DX;
+    }
+
+    public void setIP(int IP) {
+        this.IP = IP;
+    }
+
+    public void setSI(int SI) {
+        this.SI = SI;
+    }
+
+    public void setSP(int SP) {
+        this.SP = SP;
+    }
+
+    public void setSR(int SR) {
+        this.SR = SR;
+    }
+
 }
