6870427cca6d45d58b48c582f6337cfdcbe94233
Paulo Roberto Nunes da Rosa <cpte.paulornr@gmail.com>
2018-06-29 09:09:40 -0300

    Construção da classe registradores

--- src/Registradores.java
+++ src/Registradores.java
@@ -1,3 +1,12 @@
 public class Registradores{
-    
+    private int AX;
+    private int DX;
+    private int SP;
+    private int SI;
+    private int IP;
+    private int SR;
+
+    public Registradores(){
+        this.SR=0;
+    }
 }
