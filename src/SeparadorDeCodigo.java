5b60c29691724edc7830ccd43e00d250ae10a3af
ThiagoBubolz <tlabubolz@inf.ufpel.edu.br>
2018-07-29 11:38:56 -0300

    Adicionando a classe que separa o codigo entre definições de macros e programa a executar

---
+++ src/SeparadorDeCodigo.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+public class SeparadorDeCodigo {
+    private ArrayList<ArrayList<String>> codigoCompleto;
+    private ArrayList<ArrayList<String>> codigoMacros;
+    private ArrayList<ArrayList<String>> codigoExecutavel;
+
+    public SeparadorDeCodigo(ArrayList<ArrayList<String>> codigoCompleto) {
+        this.codigoCompleto = codigoCompleto;
+        this.codigoExecutavel = null;
+        this.codigoMacros = null;
+
+    }
+
+    /**
+     * Metodo que separa o codigo em definições de macros e codigo executável para uma melhor execução da máquina
+     */
+    public void SaparaCodigoMacros(){
+        boolean copiaMacro = false;
+        for(ArrayList<String> linha : this.codigoCompleto) {
+            if(eInicioDaMacro(linha))
+                copiaMacro = true;
+            if (!eFimDaMacro(linha)) {
+                copiaMacro = false;
+                this.codigoMacros.add(linha);
+            }
+            if (copiaMacro) {
+                this.codigoMacros.add(linha);
+            }else this.codigoExecutavel.add(linha);
+        }
+    }
+
+    /**
+     * Metodo que verifica se a linha contém uma definição de macro
+     * @param linha ArrayList de Strings
+     * @return true se for uma linha que define uma macro e false caso contrario
+     */
+    public boolean eInicioDaMacro(ArrayList<String> linha){
+        for (String comando : linha){
+            if (comando.equals("MACRO")) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Metodo que verifica se a linha contém uma definição de macro
+     * @param linha ArrayList de Strings
+     * @return true se for uma linha que define uma macro e false caso contrario
+     */
+    public boolean eFimDaMacro(ArrayList<String> linha){
+        for (String comando : linha){
+            if (comando.equals("ENDM")) return true;
+        }
+        return false;
+    }
+
+    public ArrayList<ArrayList<String>> getCodigoCompleto() {
+        return codigoCompleto;
+    }
+
+    public void setCodigoCompleto(ArrayList<ArrayList<String>> codigoCompleto) {
+        this.codigoCompleto = codigoCompleto;
+    }
+}
+
